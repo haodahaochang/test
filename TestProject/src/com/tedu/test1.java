896fc79c05ca2b1a9c45bad5c9da85c67f94aef7
Administrator <Administrator@YLMF-1808301135>
2020-01-03 10:35:57 +0800

    王海涛

---
+++ TestProject/src/com/tedu/test1.java
@@ -0,0 +1,13 @@
+package com.tedu;
+/**
+ * @author 作者:	扶倾
+ * @version 创建时间:	2020年1月3日	上午10:29:56
+ * @description 描述:很牛逼就对了
+ */
+public class test1 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
